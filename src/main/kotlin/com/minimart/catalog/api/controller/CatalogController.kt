8e935d3011477e0ae78b1c18e81f23a8b57c66ac
Prudhvi Arthimalla <prudhviarthimalla@gmail.com>
2025-08-16 19:13:39 -0400

    Add product creation flow with DTO, mapper, and model (#1)
    - Created CreateProductRequest, ProductMapper, Product, Category
    - Updated CatalogController
    - Renamed persistence/Product.kt to ProductDocument.kt

--- src/main/kotlin/com/minimart/catalog/api/controller/CatalogController.kt
+++ src/main/kotlin/com/minimart/catalog/api/controller/CatalogController.kt
@@ -1,6 +1,5 @@
 package com.minimart.catalog.api.controller
 
-import com.minimart.catalog.infra.persistence.Product
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
---
+++ src/main/kotlin/com/minimart/catalog/common/utils/Category.kt
@@ -0,0 +1,5 @@
+package com.minimart.catalog.common.utils
+
+enum class Category {
+    GROCERY, ELECTRONICS, CLOTHING, HOME, OTHER
+}
