620b1659bee40f5f9e34cf5eec8bb14d663a0843
Prudhvi Arthimalla <prudhviarthimalla@gmail.com>
2025-09-08 18:42:26 -0400

    Modify docker-compose and application.yml, create ProductCreated event structure

---
+++ src/main/kotlin/com/minimart/catalog/domain/events/ProductCreated.kt
@@ -0,0 +1,14 @@
+package com.minimart.catalog.domain.events
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@SerialName("com.minimart.events.ProductListedForInventoryV1")
+@Serializable
+data class ProductCreated(
+    val skuCode: String,
+    val occurredAtEpochMs: Long,
+    val eventId: String,
+    val source: String = "catalog-service",
+    val correlationId: String? = null
+)
